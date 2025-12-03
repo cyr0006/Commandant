d294a9cdc0467f1df58c77a7e90fd14c88f56a4b
Aryan <Aryan.m10@yahoo.com>
2025-12-03 22:49:18 +1100

    set up basic bot

---
+++ main.py
@@ -0,0 +1,16 @@
+import discord
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+class Client(discord.Client):
+    async def on_ready(self):
+        print(f'logged on as {self.user}!')
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = Client(intents=intents)
+client.run(TOKEN)
